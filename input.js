904349273e48b4f29024aa631c4a520e88f54e96
Abdurahman Ismail Abdi <ismail530@gmail.com>
2021-09-22 22:47:31 +0000

    input

--- input.js
+++ input.js
@@ -1,9 +1,27 @@
+
+let connection;
+
+
+
 const handleUserInput = function (key) {
   if (key === '\u0003') {
     process.exit();
   }
+  if (key === 'w'){
+    connection.write('Move: up');
+  }
+  if (key === 'a'){
+    connection.write('Move: left');
+  }
+  if (key === 's'){
+    connection.write('Move: down');
+  }
+  if (key === 'd'){
+    connection.write('Move: right');
+  }
 };
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -11,5 +29,10 @@ const setupInput = function () {
   stdin.on("data", handleUserInput);
   return stdin;
 };
+
+const w = "Move: up";
+const a = "Move: left";
+const s = "Move: down";
+const d = 'Move: right';
 
 module.exports = {setupInput};
