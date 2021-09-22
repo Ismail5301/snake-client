702a54c4fe47636e2bd904ff283a6082c88c4a66
Abdurahman Ismail Abdi <ismail530@gmail.com>
2021-09-22 19:21:10 +0000

    client.js

---
+++ client.js
@@ -0,0 +1,27 @@
+
+
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: '135.23.223.133',
+    port: '50542',
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  
+
+  conn.on("connect", () => {
+    console.log('Connecting to Server');
+    conn.write('Name: XYZ');
+  });
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
+
+module.exports = {connect};
