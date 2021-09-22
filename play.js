1dd6becfb9a737123dc7ee7eb2e5eb3c86a94b21
Abdurahman Ismail Abdi <ismail530@gmail.com>
2021-09-22 21:42:31 +0000

    updated setupinput and handleuserinput

--- play.js
+++ play.js
@@ -2,6 +2,21 @@ const net = require("net");
 const {connect} = require("./client.js");
 
 // establishes a connection with the game server
+const handleUserInput = function (key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
 
 
 connect();
+setupInput();
